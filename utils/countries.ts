c34551dcd93c55609772af9fc146cf16787e3946
Olalekan Onaolapo <70687294+B0bbycode@users.noreply.github.com>
2023-06-27 18:36:46 +0300

    countries commit

---
+++ utils/countries.ts
@@ -0,0 +1,40 @@
+const getCountries = (): string[] => {
+    const countries: string[] = [
+        'Australia',
+        'Austria',
+        'Belgium',
+        'Bulgaria',
+        'Canada',
+        'Croatia',
+        'Cyprus',
+        'Czech',
+        'Denmark',
+        'Estonia',
+        'Finland',
+        'France',
+        'Germany',
+        'Greece',
+        'Hungary',
+        'Ireland',
+        'Italy',
+        'Lithuania',
+        'Luxembourg',
+        'Mexico',
+        'Netherlands',
+        'New Zealand',
+        'Norway',
+        'Poland',
+        'Portugal',
+        'Spain',
+        'Sweden',
+        'Switzerland',
+        'United Kingdom',
+        'United States',
+      // countries we operate in
+    ];
+  
+    return countries;
+  };
+  
+  export default getCountries;
+  
