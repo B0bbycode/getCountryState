3e2443098eccc1761a553a52c4f62b53470d4c9d
Olalekan Onaolapo <70687294+B0bbycode@users.noreply.github.com>
2023-06-28 11:35:51 +0300

    commit changes

--- utils/states.ts
+++ utils/states.ts
@@ -9,6 +9,22 @@ type StateMap = {
         'Austria': ['Vienna', 'Upper Austria', 'Styria', 'Tyrol'],
         'Belgium': ['Brussels', 'Flanders', 'Wallonia'],
         'Bulgaria': ['Sofia City Province', 'Plovdiv', 'Varna', 'Burgas'],
+        'Canada': ['Ontario', 'Quebec', 'British Columbia', 'Alberta'],
+        'Croatia': ['Zagreb', 'Split-Dalmatia County', 'Dubrovnik-Neretva County', 'Istria County'],
+        'Cyprus': ['Nicosia', 'Limassol', 'Larnaca', 'Paphos'],
+        'Czech Republic': ['Prague', 'Central Bohemia', 'South Bohemia', 'Moravia-Silesia'],
+        'Denmark': ['Capital Region of Denmark', 'Central Denmark Region', 'Region of Southern Denmark', 'North Denmark Region'],
+        'Estonia': ['Harju County', 'Tartu County', 'Ida-Viru County', 'Pärnu County'],
+        'Finland': ['Uusimaa', 'Pirkanmaa', 'Varsinais-Suomi', 'Kanta-Häme'],
+        'France': ['Île-de-France', 'Provence-Alpes-Côte d\'Azur', 'Auvergne-Rhône-Alpes'],
+        'Germany': ['Berlin', 'Bavaria', 'Hamburg', 'North Rhine-Westphalia'],
+        'Greece': ['Attica', 'Central Macedonia', 'Crete', 'Peloponnese'],
+        'Hungary': ['Budapest', 'Pest County', 'Debrecen', 'Szeged'],
+        'Ireland': ['Dublin', 'Cork', 'Galway', 'Limerick'],
+        'Italy': ['Lazio', 'Lombardy', 'Campania', 'Sicily'],
+        'Lithuania': ['Vilnius County', 'Kaunas County', 'Klaipėda County', 'Šiauliai County'],
+        'Luxembourg': ['Luxembourg District', 'Grevenmacher District', 'Diekirch District'],
+    // Add more country-state mappings as needed
     };
   
     // Check if the selected country exists in the state map
